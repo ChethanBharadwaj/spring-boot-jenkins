c87ab0c1054e0e708a4d811d38a0e7cd8ebbe350
ChethanBharadwaj <chethanmbharadwaj@gmail.com>
2024-06-22 23:09:26 +0530

    Second Logger removed in Test

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ EXPOSE 8080
 ADD target/spring-boot-jenkins.jar spring-boot-jenkins.jar
 
 
-ENTRYPOINT [ "java", "-jar", "spring-boot-docker-maven.jar" ]
+ENTRYPOINT [ "java", "-jar", "spring-boot-jenkins.jar" ]
 
--- src/test/java/com/jenkins/demo/SpringBootJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/demo/SpringBootJenkinsApplicationTests.java
@@ -15,7 +15,6 @@ class SpringBootJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("secong log statement Test case executing..");
 		assertThat(true);
 	}
 
